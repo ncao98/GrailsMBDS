f3a0a15bfb10638999c797147bfb1969751ba016
Nicolas CAO <n.cao@net.estia.fr>
2020-10-14 09:37:01 +0200

    Permit update

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -6,16 +6,17 @@ grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'tp.mbds.com.Us
 grails.plugin.springsecurity.authority.className = 'tp.mbds.com.Role'
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/**',               access: ['ROLE_ADMIN','ROLE_MODERATOR']],
+	[pattern: '/',               access: ['permitAll']],
 	[pattern: '/error',          access: ['ROLE_ADMIN','ROLE_MODERATOR']],
-	[pattern: '/index',          access: ['ROLE_ADMIN','ROLE_MODERATOR']],
-	[pattern: '/index.gsp',      access: ['ROLE_ADMIN','ROLE_MODERATOR']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
 	[pattern: '/shutdown',       access: ['ROLE_ADMIN','ROLE_MODERATOR']],
 	[pattern: '/assets/**',      access: ['ROLE_ADMIN','ROLE_MODERATOR']],
 	[pattern: '/**/js/**',       access: ['ROLE_ADMIN','ROLE_MODERATOR']],
 	[pattern: '/**/css/**',      access: ['ROLE_ADMIN','ROLE_MODERATOR']],
 	[pattern: '/**/images/**',   access: ['ROLE_ADMIN','ROLE_MODERATOR']],
-	[pattern: '/**/favicon.ico', access: ['ROLE_ADMIN','ROLE_MODERATOR']]
+	[pattern: '/**/favicon.ico', access: ['ROLE_ADMIN','ROLE_MODERATOR']],
+	[pattern: '/saleAd/index',	 access: ['permitAll']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
